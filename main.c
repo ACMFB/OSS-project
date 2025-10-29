b50b295ccf0bfb208695ce59b9845f596927caa3
EUIHEOK <130281371+ACMFB@users.noreply.github.com>
2025-10-29 13:09:55 +0900

    Update main.c - new main()

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 int main() {
-  for (int i = 1; i <= 5; i++) { 
-    printf("Github training %d\n", i);
-  }
-  return 0;
+    char c;
+    int a, b;
+    while (1) {
+        printf("Enter 2 integers>> ");
+        scanf_s("%d%d", &a, &b);
+        printf("Enter operator>> ");
+        scanf_s(" %c", &c);
+
+        if (c == '+') {
+            printf("%d\n", plus(a, b));
+        }
+        else if (c == '-') {
+            printf("%d\n", minus(a, b));
+        }
+        else if (c == '*') {
+            printf("%d\n", time(a, b));
+        }
+        else if (c == '/') {
+            printf("%d\n", divided(a, b));
+        }
+    }
+    return 0;
 }
